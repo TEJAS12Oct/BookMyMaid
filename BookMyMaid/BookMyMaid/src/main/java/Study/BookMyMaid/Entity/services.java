6da5d956fe1e5bb33c09d8b2c223ac4bcf95529e
mayurnigade <mayurnigade10@gmail.com>
2022-09-15 15:13:16 +0530

    services Entity

---
+++ BookMyMaid/BookMyMaid/src/main/java/Study/BookMyMaid/Entity/services.java
@@ -0,0 +1,92 @@
+package Study.BookMyMaid.Entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "services")
+public class services {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "servicesId")
+	private int servicesId;
+
+	@Column(name = "servicesName")
+	private String servicesName;
+
+	@Column(name = "baseCharges")
+	private int baseCharges;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "maidId")
+	maid maid;
+
+//	@OneToMany(mappedBy = "services", cascade = CascadeType.ALL)
+//	private List<maid> maid = new ArrayList<>();
+
+	public services() {
+		super();
+	}
+
+	public services(int servicesId, String servicesName, int baseCharges) {
+		super();
+		this.servicesId = servicesId;
+		this.servicesName = servicesName;
+		this.baseCharges = baseCharges;
+	}
+
+	public services(int servicesId, String servicesName, int baseCharges, Study.BookMyMaid.Entity.maid maid) {
+		super();
+		this.servicesId = servicesId;
+		this.servicesName = servicesName;
+		this.baseCharges = baseCharges;
+		this.maid = maid;
+	}
+
+	public int getServicesId() {
+		return servicesId;
+	}
+
+	public void setServicesId(int servicesId) {
+		this.servicesId = servicesId;
+	}
+
+	public String getServicesName() {
+		return servicesName;
+	}
+
+	public void setServicesName(String servicesName) {
+		this.servicesName = servicesName;
+	}
+
+	public int getBaseCharges() {
+		return baseCharges;
+	}
+
+	public void setBaseCharges(int baseCharges) {
+		this.baseCharges = baseCharges;
+	}
+
+	public maid getMaid() {
+		return maid;
+	}
+
+	public void setMaid(maid maid) {
+		this.maid = maid;
+	}
+
+	@Override
+	public String toString() {
+		return "services [servicesId=" + servicesId + ", servicesName=" + servicesName + ", baseCharges=" + baseCharges
+				+ ", maid=" + maid + "]";
+	}
+
+}
