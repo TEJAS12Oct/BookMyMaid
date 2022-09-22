b73e11d63af33c8346abeafc0c3b66ab143dd5e2
Atharvab30 <106829799+Atharvab30@users.noreply.github.com>
2022-09-22 19:53:55 +0530

    Confirm page uploaded

---
+++ react components/confirm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Confirm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="fixed-bottom">
+                <h1>Your booking has been confirmed.</h1>
+            </div>
+        )
+    }
+}
+
+export default Confirm;
